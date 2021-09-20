9d3af6ecb3f04fe812b56cb6824ac2a09c8ae19e
Thorsten Diederichs <DiederichsT@bfwob.de>
2021-09-20 14:45:33 +0200

    Änderungen

--- P2021_09_20_Web_FIA35/Startup.cs
+++ P2021_09_20_Web_FIA35/Startup.cs
@@ -16,6 +16,7 @@ namespace P2021_09_20_Web_FIA35
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,12 @@ namespace P2021_09_20_Web_FIA35
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "Startseite",
+                    //pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Otto}/{action=Index}/{id?}");
+                    
+
             });
         }
     }
