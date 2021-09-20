c2a831e13303c7f714fbb7c04d546c577fdcd274
Thorsten Diederichs <DiederichsT@bfwob.de>
2021-09-20 14:07:07 +0200

    Projektdateien hinzufügen.

---
+++ P2021_09_20_Web_FIA35/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2021_09_20_Web_FIA35.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Ganz viel Code könnte hier drinstehen
+            return View();
+        }
+
+        public IActionResult Willi()
+        {
+            // Ganz viel Code könnte hier drinstehen
+            return View();
+        }
+
+    }
+}
---
+++ P2021_09_20_Web_FIA35/Controllers/OttoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2021_09_20_Web_FIA35.Controllers
+{
+    public class OttoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
